e9eb7fa0970b00e2be1aba8f8179fc2d28f08758
guyddogl <guyddogl@gmail.com>
2022-07-13 09:57:23 -0300

    Refatora função getUrlParameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,11 @@
-function getUrlParameter() {
-  let urlParams = new URLSearchParams(window.location.search);
-  let urlObj = {
-    utm_id: urlParams.get('utm_id'), 
-    utm_campaign: urlParams.get('utm_campaign'), 
-    utm_source: urlParams.get('utm_source'), 
-    utm_medium: urlParams.get('utm_medium'), 
-    utm_term: urlParams.get('utm_term'), 
-    utm_content: urlParams.get('utm_content'), 
-    src: urlParams.get('src')
-  };
-  let utms ="";
-  for (let index = 0; index < Object.keys(urlObj).length; index += 1) {
-      if (Object.values(urlObj)[index] !== null) {
-          utms += `&${Object.keys(urlObj)[index]}=${Object.values(urlObj)[index]}`;
-      }
-  }
-  let urlOriginal = document.getElementById('checkout').href;
-  return document.getElementById('checkout').href=`${urlOriginal}${utms}`;
+const getUrlParameters = () => {
+  const urlParameters = new URLSearchParams(window.location.search);
+  let parameters = '';
+  for(let entry of urlParameters.entries()) parameters += `&${entry[0]}=${entry[1]}`;
+  const urlCheckout = document.getElementById('button-checkout').href;
+  return document.getElementById('button-checkout').href=`${urlCheckout}${parameters}`;
+};
+
+window.onload = () => {
+  getUrlParameters();
 }
-getUrlParameter();
