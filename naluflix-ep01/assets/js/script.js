7159638b2526fe55f5055b6bd7b406888beae31e
guyddogl <guyddogl@gmail.com>
2022-07-17 09:50:18 -0300

    Altera getUrlParameters para concacnar os parâmetros da url dentro de src

--- naluflix-ep01/assets/js/script.js
+++ naluflix-ep01/assets/js/script.js
@@ -1,11 +1,9 @@
 const getUrlParameters = () => {
   const urlParameters = new URLSearchParams(window.location.search);
-  let parameters = '';
-  for(let entry of urlParameters.entries()) parameters += `&${entry[0]}=${entry[1]}`;
+  let parametersKeysAndValues = '';
+  for(let entry of urlParameters.entries()) parametersKeysAndValues += `&${entry[0]}=${entry[1]}`;
+  let parametersValues = '';
+  for(let entry of urlParameters.entries()) parametersValues.length === 0 ? parametersValues += `${entry[1]}` : parametersValues += `-${entry[1]}`;
   const urlCheckout = document.getElementById('button-checkout').href;
-  return document.getElementById('button-checkout').href=`${urlCheckout}${parameters}`;
+  return document.getElementById('button-checkout').href=`${urlCheckout}&src=${parametersValues}${parametersKeysAndValues}`;
 };
-
-window.onload = () => {
-  getUrlParameters();
-}
